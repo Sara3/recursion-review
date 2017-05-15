9cca6f5d40b94534c6722c1e2a7204d330f4c55b
Hack Reactor Student <student@hackreactor.com>
2017-05-15 15:46:43 -0700

    stringify

--- spec/stringifyJSONSpec.js
+++ spec/stringifyJSONSpec.js
@@ -5,14 +5,31 @@ describe('stringifyJSON', function() {
     stringifiableObjects.forEach(function(test) {
       var expected = JSON.stringify(test);
       var result = stringifyJSON(test);
-      expect(result).to.equal(FILL_ME_IN);
+      expect(result).to.equal(expected);
     });
 
     unstringifiableValues.forEach(function(obj) {
       var expected = JSON.stringify(obj);
       var result = stringifyJSON(obj);
-      expect(result).to.equal(FILL_ME_IN);
+      expect(result).to.equal(expected);
     });
 
   });
+  it(" stringify stirng, num, bool, null", function(){
+    const arr = [ '5', 5, true, false, null];
+    arr.forEach(function(el) {
+      var expected = JSON.stringify(el);
+      var result = stringifyJSON(el);
+      expect(result).to.equal(expected);
+    });
+  });
+
+  it(" stringify array", function(){
+    const arr = [ '5', 5, true, false, null];
+      var expected = JSON.stringify(arr);
+      var result = stringifyJSON(arr);
+      expect(result).to.equal(expected);
+
+  });
+  
 });
